dd217b8a93a83bdec0f37bb249623d8942372031
Caterina Fuster-Barceló <100411909@alumnos.uc3m.es>
2025-01-10 11:27:07 +0100

    toy classificator working

--- models/classificator.py
+++ models/classificator.py
@@ -27,7 +27,7 @@ class SimpleCNN(nn.Module):
         # Define layers
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(32 * 64 * 64, 128)
+        self.fc1 = nn.Linear(204800, 128)
         self.fc2 = nn.Linear(128, num_classes)
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
