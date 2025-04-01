86cb75a1339e870473eac40ae1ac1b6a3202e531
Laura Rodrigo Muñoz <laurarodrigo7@gmail.com>
2025-04-01 18:53:06 +0200

    new training set results MRC - working

--- main_segmentator.py
+++ main_segmentator.py
@@ -21,7 +21,7 @@ from models.segmentator.segmentator import Segmentator, UNet, UNet_new, UNetOpti
 # Configuration Parameters
 SAVE_RESULTS = True  # Toggle to save results
 SAVE_WEIGHTS = True
-NUM_EPOCHS = 25  # Number of training epochs
+NUM_EPOCHS = 15  # Number of training epochs
 
 LEARNING_RATE = 1e-4  # Learning rate for the optimizer
 BATCH_SIZE = 3  # Batch size for training
@@ -45,8 +45,8 @@ else:
     # PEI_IMAGES_FOLDER = r"D:\Desktop\NORMALIZED_CROPPED_DATASET\images\PEI_images_Z"
     # PEI_LABELS_FOLDER = r"D:\Desktop\NORMALIZED_CROPPED_DATASET\labels\PEI_labels_Z"
     
-    MRC_IMAGES_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\images\\normalized_images_MRC'
-    MRC_LABELS_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\labels\\labels_MRC'
+    MRC_IMAGES_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\images\\flipped_images_MRC'
+    MRC_LABELS_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\labels\\flipped_labels_MRC'
     PEI_IMAGES_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\images\\flipped_clean_images_PEI' #PEI_images_Z #normalized_images_PEI
     PEI_LABELS_FOLDER = 'C:\\Users\\TFM1\\Documents\\Data\\EHydropsAnalysis\\NORMALIZED_CROPPED_DATASET\\labels\\flipped_clean_labels_PEI'
 
@@ -115,12 +115,21 @@ train_patients = [
     "PAC34", "PAC9", "PAC38", "PAC72", "PAC31", "PAC44", "PAC62", "PAC42", "PAC40", "PAC89", "PAC47", "PAC78", 
     "PAC13", "PAC37", "PAC24", "PAC19", "PAC66", "PAC69", "PAC53", "PAC8", "PAC35", "PAC74", "PAC3", "PAC17", 
     "PAC39", "PAC51", "PAC23", "PAC79", "PAC25", "PAC6", "PAC7", "PAC61", "PAC49", "PAC83", "PAC10", "PAC84", 
-    "PAC22", "PAC75"
+    "PAC22", "PAC75", "PAC153", "PAC110", "PAC125", "PAC178", "PAC132", "PAC107", "PAC188", "PAC106", "PAC111", 
+    "PAC162", "PAC119", "PAC116", "PAC109", "PAC120", "PAC161", "PAC148", "PAC179", "PAC115", "PAC165", "PAC172", 
+    "PAC113", "PAC169", "PAC151", "PAC112", "PAC117", "PAC131", "PAC149", "PAC177", "PAC157", "PAC123", "PAC121", 
+    "PAC141", "PAC130", "PAC159", "PAC136", "PAC164", "PAC168", "PAC190", "PAC142", "PAC103", "PAC147", "PAC102", 
+    "PAC156", "PAC176", "PAC122", "PAC105", "PAC146", "PAC158", "PAC173", "PAC187", "PAC154", "PAC186", "PAC139", 
+    "PAC152", "PAC174", "PAC191", "PAC185", "PAC108", "PAC180", "PAC134", "PAC135", "PAC129", "PAC181", "PAC155", 
+    "PAC184", "PAC143", "PAC189", "PAC128", "PAC133", "PAC183", "PAC144", "PAC126"
 ]
 
 val_patients = [
-    "PAC45", "PAC21", "PAC1", "PAC87", "PAC58", "PAC85", "PAC54", "PAC90", "PAC26"
+    "PAC45", "PAC21", "PAC1", "PAC87", "PAC58", "PAC85", "PAC54", "PAC90", "PAC26", "PAC114", "PAC118", "PAC163", 
+    "PAC150", "PAC182", "PAC145", "PAC137", "PAC138", "PAC104", "PAC171", "PAC127", "PAC140", "PAC167", "PAC166", 
+    "PAC124", "PAC175", "PAC160", "PAC170"
 ]
+
 
 test_patients = [
     "PAC77", "PAC65", "PAC30", "PAC28", "PAC81", "PAC88", "PAC5", "PAC55", "PAC76", "PAC12", "PAC70", "PAC14", 
