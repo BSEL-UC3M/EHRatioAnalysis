360d45f02bca0fdfb46b5af93ef0ebdf65921601
claudiacastrillon <160623790+claudiacastrillon@users.noreply.github.com>
2025-07-01 14:14:57 +0200

    convert to tiff new patients

--- utils/convert_to_tiff.py
+++ utils/convert_to_tiff.py
@@ -5,8 +5,8 @@ from matplotlib.image import imsave
 from pydicom.errors import InvalidDicomError
 
 # Define input and output folders
-input_folder = '/Users/claudiacastrillonalvarez/Desktop/PACIENTES_97_A_102/PACIENTE_102/DICOM/25052215/01300000'
-output_folder = '/Users/claudiacastrillonalvarez/Desktop/new_patients_tiff/pac_102_tiff'
+input_folder = '/Users/claudiacastrillonalvarez/Desktop/PACIENTES_103_A_107/PACIENTE_107/DICOM/25061210/04240000'
+output_folder = '/Users/claudiacastrillonalvarez/Desktop/patients_tiff_103_107/PACIENTE_107'
 
 # Create output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
